6848a4aad15ec6a8b1b4c13547de1197637d600a
catweaver <Simon@Simons-MacBook-Air-3.local>
2015-01-11 20:04:02 -0600

    Fix for plot 3

--- plot3.R
+++ plot3.R
@@ -23,8 +23,15 @@ setnames(DT,variablenames)
 #Create third plot
 DT$DateTime<- as.POSIXct(paste(DT$Date,DT$Time), format="%d/%m/%Y %H:%M:%S")
 png(file="plot3.png")
-with(DT, {
-        plot(DateTime, Sub_metering_1, type="l",ylab="Energy sub metering",
-             xlab="")
-})
-dev.off
+with(DT, 
+     {plot(DateTime, Sub_metering_1, type="l",ylab="Energy sub metering",
+             xlab="") ## Create plot with first line
+      lines(DateTime,Sub_metering_2, col="red") ## Add second line to plot
+      lines(DateTime,Sub_metering_3, col="blue") ## Add 3rd line to plot
+      legend("topright",
+             c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),
+             lty=c(1,1,1),
+             col=c("black", "red", "blue")) ## Create legend
+           })
+dev.off()
+
